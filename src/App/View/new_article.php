4d2f76dfe153f59e40ccd1c4c87596ba71bb202c
José Manuel Cornelio <jose@cornelioweb.com>
2024-01-30 09:44:08 -0400

    Mejora de UI: Cambiar input por text-area en el campo de descripción

--- src/App/View/new_article.php
+++ src/App/View/new_article.php
@@ -2,8 +2,9 @@
     <h1 class="text-center">Create New Article</h1>
     <form action="/article/new" method="post" class="flex flex-col gap-3 mt-5">
         <input name="name" type="text" placeholder="Name" class="input input-bordered w-full " />
-        <input name="description" type="text" placeholder="Short Description" class="input input-bordered w-full " />
+        <!-- <input  type="text"  class="input input-bordered w-full " /> -->
+        <textarea name="description" class="textarea textarea-bordered" placeholder="Short Description"></textarea>
         <input name="price" type="number" placeholder="Price" class="input input-bordered w-full " />
-        <input type="submit" value="Create" class="btn btn-primary" />
+        <input type="submit" value="Save" class="btn btn-primary" />
     </form>
 </div>
